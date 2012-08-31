022ab33756507dbddf70fd20ba0d150be77973a8
Arnav Singh <arnavion@gmail.com>
2012-08-31 09:07:21 +0800

    Fixed smart mode blowing up for temperatures below 45 degrees C due to logic error

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ class Settings:
 	COLORS = {0: Qt.GlobalColor.cyan, 55: Qt.GlobalColor.yellow, 65: QColor('orange'), 90: Qt.GlobalColor.red}
 	
 	"""The fan levels for SMART mode. The key is the temperature in Celsius, and the value is the fan level."""
-	LEVELS = {45: '0', 55: '1', 65: '3', 80: '7', 90: 'disengaged'}
+	LEVELS = {0: '0', 55: '1', 65: '3', 80: '7', 90: 'disengaged'}
 	
 	"""The time in seconds between updates of the thermal sensors and fan sensor."""
 	UPDATE_INTERVAL = 5
