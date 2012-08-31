033494c097b4fbda1ce740e7d9be1752d41fe30a
Arnav Singh <arnavion@gmail.com>
2012-08-31 08:39:47 +0800

    Moved most UI creation code to a .ui file

--- tpfc.py
+++ tpfc.py
@@ -6,19 +6,19 @@ import sys
 
 from PySide.QtCore import *
 from PySide.QtGui import *
+from PySide.QtUiTools import QUiLoader
 
 from acpi import Temperatures, Fan
 from settings import Settings
 
 
-class TPFCWindow(QWidget):
-	"""
-	Main window of application.
-	
-	"""
-	
+class TPFCUiLoader(QUiLoader):
 	def __init__(self):
-		super().__init__(windowTitle = 'TPFanControl')
+		# Create the application. The application will not close even if its windows are closed.
+		app = QApplication(sys.argv)#, quitOnLastWindowClosed = False)
+		
+		super().__init__()
+		self.registerCustomWidget(TPFCWindow)
 		
 		# Holds the fan levels' temperatures in sorted order
 		self._levelTemps = sorted(Settings.LEVELS.keys())
@@ -27,127 +27,47 @@ class TPFCWindow(QWidget):
 		
 		self._smartMode = False
 		
-		mainLayout = QHBoxLayout()
-		self.setLayout(mainLayout)
+		f = QFile('tpfc.ui')
+		f .open(QFile.ReadOnly)
+		self.load(f)
+		f.close()
 		
-		tempsGB = QGroupBox('Temperatures')
-		mainLayout.addWidget(tempsGB)
-		
-		tempsLayout = QVBoxLayout()
-		tempsGB.setLayout(tempsLayout)
-		
-		tempsTable = QTableWidget(len(Settings.SENSOR_NAMES), 3, focusPolicy = Qt.NoFocus, selectionMode = QTableWidget.NoSelection, showGrid = False)
-		tempsLayout.addWidget(tempsTable)
-		tempsTable.horizontalHeader().hide()
-		tempsTable.verticalHeader().setResizeMode(QHeaderView.ResizeToContents)
+		self._tempsTable.setRowCount(len(Settings.SENSOR_NAMES))
+		self._tempsTable.verticalHeader().setResizeMode(QHeaderView.ResizeToContents)
 		
 		# Map of sensor name to the corresponding GUI label which displays the temperature of that sensor
 		self._valueLabels = {}
 		for (i, name) in enumerate(Settings.SENSOR_NAMES):
-			tempsTable.setItem(i, 0, QTableWidgetItem(name))
+			self._tempsTable.setItem(i, 0, QTableWidgetItem(name))
 			
 			tempLabel = QTableWidgetItem()
-			tempsTable.setItem(i, 1, tempLabel)
+			self._tempsTable.setItem(i, 1, tempLabel)
 			self._valueLabels[name] = tempLabel
 			
-			tempsTable.setItem(i, 2, QTableWidgetItem('\xB0C'))
+			self._tempsTable.setItem(i, 2, QTableWidgetItem('\xB0C'))
 		
-		visibleTempsLayout = QHBoxLayout()
-		tempsLayout.addLayout(visibleTempsLayout)
+		self._activeButton.toggled.connect(self.toggleTempSensorsVisibility)
+		self.toggleTempSensorsVisibility()
 		
-		# The 'all' radio button displays all the sensors
-		allButton = QRadioButton('all')
-		visibleTempsLayout.addWidget(allButton)
+		self._biosModeButton.clicked.connect(self.enableBIOSMode)
+		self._smartModeButton.clicked.connect(self.enableSmartMode)
 		
-		# The 'active' radio button displays only those sensors which haven't been hidden
-		activeButton = QRadioButton('active')
-		visibleTempsLayout.addWidget(activeButton)
-		# If the 'active' button is toggled, show or hide the rows corresponding to the hidden sensors, based on whether the 'active' button is checked or not
-		activeButton.toggled.connect(lambda: [tempsTable.setRowHidden(Settings.SENSOR_NAMES.index(name), activeButton.isChecked()) for name in Settings.HIDDEN_TEMPS])
-		# Enable the 'active' button by default
-		activeButton.setChecked(True)
-		
-		tpfcGB = QGroupBox('TPFanControl')
-		mainLayout.addWidget(tpfcGB)
-		
-		tpfcLayout = QVBoxLayout()
-		tpfcGB.setLayout(tpfcLayout)
-		
-		stateLayout = QHBoxLayout()
-		tpfcLayout.addLayout(stateLayout)
-		
-		stateLayout.addWidget(QLabel('State'))
-		# Displays the level of the fan
-		self._fanStateLabel = QLabel()
-		stateLayout.addWidget(self._fanStateLabel)
-		
-		speedLayout = QHBoxLayout()
-		tpfcLayout.addLayout(speedLayout)
-		
-		speedLayout.addWidget(QLabel('Speed'))
-		# Displays the speed of the fan
-		self._fanSpeedLabel = QLabel(alignment = Qt.AlignRight | Qt.AlignVCenter)
-		speedLayout.addWidget(self._fanSpeedLabel)
-		speedLayout.addWidget(QLabel('RPM'))
-		
-		modeLayout = QHBoxLayout()
-		tpfcLayout.addLayout(modeLayout)
-		
-		modeLayout.addWidget(QLabel('Mode'))
-		
-		modeOptionsLayout = QVBoxLayout()
-		modeLayout.addLayout(modeOptionsLayout)
-		
-		fanButtonsGroup = QButtonGroup()
-		# Clicking the 'BIOS' button enables BIOS mode.
-		biosModeButton = QRadioButton('BIOS', clicked = self.enableBIOSMode)
-		modeOptionsLayout.addWidget(biosModeButton)
-		fanButtonsGroup.addButton(biosModeButton)
-		
-		# Clicking the 'Smart' button enables Smart mode.
-		smartModeButton = QRadioButton('Smart', clicked = self.enableSmartMode)
-		modeOptionsLayout.addWidget(smartModeButton)
-		fanButtonsGroup.addButton(smartModeButton)
-
-		manualModeLayout = QHBoxLayout()
-		modeOptionsLayout.addLayout(manualModeLayout)
-		
-		# Clicking the 'Manual' button enables manual mode with the level selected in the dropdown.
-		manualModeButton = QRadioButton('Manual', clicked = lambda: self.enableManualMode(manualModeCombo.currentText()))
-		manualModeLayout.addWidget(manualModeButton)
-		fanButtonsGroup.addButton(manualModeButton)
-		
-		# This dropdown holds the possible fan levels
-		manualModeCombo = QComboBox()
-		manualModeLayout.addWidget(manualModeCombo)
 		for speed in sorted(Fan.FIRMWARE_TO_HWMON):
-			manualModeCombo.addItem(speed)
-		manualModeCombo.addItem('full-speed')
-		manualModeCombo.setCurrentIndex(len(Fan.FIRMWARE_TO_HWMON))
+			self._manualModeCombo.addItem(speed)
+		self._manualModeCombo.addItem('full-speed')
+		self._manualModeCombo.setCurrentIndex(len(Fan.FIRMWARE_TO_HWMON))
 		# Changing the selected level changes the fan level immediately if manual mode is enabled
-		manualModeCombo.currentIndexChanged.connect(lambda: self.enableManualMode(manualModeCombo.currentText()) if manualModeButton.isChecked() else None)
+		self._manualModeCombo.currentIndexChanged.connect(lambda: self.enableManualMode(self._manualModeCombo.currentText()) if self._manualModeButton.isChecked() else None)
+		
+		self._manualModeButton.clicked.connect(lambda: self.enableManualMode(self._manualModeCombo.currentText()) if self._manualModeButton.isChecked() else None)
 		
 		# Start off the application in auto mode if the fan was already in auto mode, else set it to smart mode
 		if Fan.read().level == 'auto':
-			biosModeButton.setChecked(True)
+			self._biosModeButton.setChecked(True)
 		else:
-			smartModeButton.setChecked(True)
+			self._smartModeButton.setChecked(True)
 		
-		# The system tray icon, which also handles the window icon
-		self._systemTrayIcon = TPFCTrayIcon(self)
-		self._systemTrayIcon.activated.connect(lambda reason: self.toggleVisibility() if reason == QSystemTrayIcon.ActivationReason.Trigger else None)
-		self._systemTrayIcon.show()
-		
-		# The context menu for the system tray icon
-		trayIconMenu = QMenu(self)
-		self._systemTrayIcon.setContextMenu(trayIconMenu)
-		# The first entry in the context menu hides or shows the main window
-		self._restoreHideAction = QAction('Hide', self, triggered = self.toggleVisibility)
-		trayIconMenu.addAction(self._restoreHideAction)
-		# The second entry in the context menu quits the program
-		trayIconMenu.addAction(QAction('Quit', self, triggered = QCoreApplication.instance().quit))
-		
-		self.show()
+		self._window.show()
 		
 		# Set up updating the fan and temperature regularly, using the update interval specified in the settings
 		timer = QTimer(self)
@@ -158,36 +78,22 @@ class TPFCWindow(QWidget):
 		
 		# If the fan level is not modificable, show a warning notification balloon from the system tray icon and disable the fan level controls
 		if not Fan.isWritable():
-			self._systemTrayIcon.showMessage('Warning', 'TPFanControl does not have write access to the ACPI interface. Fan speed will be read-only.', QSystemTrayIcon.MessageIcon.Warning)
-			for control in (biosModeButton, smartModeButton, manualModeButton, manualModeCombo):
+			self._window._systemTrayIcon.showMessage('Warning', 'TPFanControl does not have write access to the ACPI interface. Fan speed will be read-only.', QSystemTrayIcon.MessageIcon.Warning)
+			for control in (self._biosModeButton, self._smartModeButton, self._manualModeButton, self._manualModeCombo):
 				control.setEnabled(False)
+		
+		# Start the event loop
+		sys.exit(app.exec_())
 	
-	def setVisible(self, visible):
-		super().setVisible(visible)
+	def createWidget(self, className, parent = None, name = ''):
+		if className == 'TPFCWindow':
+			result = TPFCWindow(parent, objectName = name)
+		else:
+			result = super().createWidget(className, parent, name)
 		
-		# Toggle the text of the 'Restore'/'Hide' system tray context menu entry
-		self._restoreHideAction.setText('Hide' if visible else 'Restore')
+		setattr(self, '_' + name, result)
 		
-		# If the window is being restored, force it to not be minimized
-		if visible and self.windowState() == Qt.WindowMinimized:
-			self.setWindowState(Qt.WindowNoState)
-	
-	def toggleVisibility(self):
-		"""
-		Toggle the visibility of the main window.
-		
-		"""
-		
-		self.setVisible(not self.isVisible())
-	
-	def closeEvent(self, event):
-		"""
-		Suppress the window's close event and hide it instead. The application only quits when the 'Quit' system tray context menu entry is selected.
-		
-		"""
-		
-		self.hide()
-		event.ignore()
+		return result
 	
 	def update(self):
 		"""
@@ -220,7 +126,7 @@ class TPFCWindow(QWidget):
 		# Find the item in the dictionary for the highest temperature
 		maxTemp = max((item for item in temps.items() if item[0] not in Settings.HIDDEN_TEMPS), key = operator.itemgetter(1))
 		# ... and tell the system tray icon to update itself and the window icon with the new temperature, sensor and background color
-		self._systemTrayIcon.update(maxTemp[0], maxTemp[1], Settings.COLORS[self._colorTemps[bisect.bisect_left(self._colorTemps, maxTemp[1]) - 1]])
+		self._window._systemTrayIcon.update(maxTemp[0], maxTemp[1], Settings.COLORS[self._colorTemps[bisect.bisect_left(self._colorTemps, maxTemp[1]) - 1]])
 		
 		# Return the highest temperature value
 		return maxTemp[1]
@@ -238,6 +144,10 @@ class TPFCWindow(QWidget):
 		# If the fan is on manual or smart mode, reset the watchdog timer by setting the same level as the current one
 		if fan.level != 'auto':
 			Fan.setLevel(fan.level)
+	
+	def toggleTempSensorsVisibility(self):
+		for name in Settings.HIDDEN_TEMPS:
+			self._tempsTable.setRowHidden(Settings.SENSOR_NAMES.index(name), self._activeButton.isChecked())
 	
 	def enableBIOSMode(self):
 		"""
@@ -274,6 +184,57 @@ class TPFCWindow(QWidget):
 		
 		Fan.setLevel(level)
 		self.updateFan()
+
+
+class TPFCWindow(QWidget):
+	"""
+	Main window of application.
+	
+	"""
+	
+	def __init__(self, parent, **kwargs):
+		super().__init__(parent, **kwargs)
+		
+		# The system tray icon, which also handles the window icon
+		self._systemTrayIcon = TPFCTrayIcon(self)
+		self._systemTrayIcon.activated.connect(lambda reason: self.toggleVisibility() if reason == QSystemTrayIcon.ActivationReason.Trigger else None)
+		self._systemTrayIcon.show()
+		
+		# The context menu for the system tray icon
+		trayIconMenu = QMenu(self)
+		self._systemTrayIcon.setContextMenu(trayIconMenu)
+		# The first entry in the context menu hides or shows the main window
+		self._restoreHideAction = QAction('Hide', self, triggered = self.toggleVisibility)
+		trayIconMenu.addAction(self._restoreHideAction)
+		# The second entry in the context menu quits the program
+		trayIconMenu.addAction(QAction('Quit', self, triggered = QCoreApplication.instance().quit))
+	
+	def toggleVisibility(self):
+		"""
+		Toggle the visibility of the main window.
+		
+		"""
+		
+		self.setVisible(not self.isVisible())
+	
+	def setVisible(self, visible):
+		super().setVisible(visible)
+		
+		# Toggle the text of the 'Restore'/'Hide' system tray context menu entry
+		self._restoreHideAction.setText('Hide' if visible else 'Restore')
+		
+		# If the window is being restored, force it to not be minimized
+		if visible and self.windowState() == Qt.WindowMinimized:
+			self.setWindowState(Qt.WindowNoState)
+	
+	def closeEvent(self, event):
+		"""
+		Suppress the window's close event and hide it instead. The application only quits when the 'Quit' system tray context menu entry is selected.
+		
+		"""
+		
+		self.hide()
+		event.ignore()
 
 
 class TPFCTrayIcon(QSystemTrayIcon):
@@ -392,12 +353,8 @@ class TPFCIconEngine(QIconEngineV2):
 
 
 def main():
-	# Create the application. The application will not close even if its windows are closed.
-	app = QApplication(sys.argv, quitOnLastWindowClosed = False)
 	# Create the main window
-	TPFCWindow()
-	# Start the event loop
-	sys.exit(app.exec_())
+	TPFCUiLoader()
 
 if __name__ == '__main__':
 	main()
