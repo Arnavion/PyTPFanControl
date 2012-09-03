1a4bbd1d348d8e0c2d52e6d0b5e62f045bc300b3
Arnav Singh <arnavion@gmail.com>
2012-09-03 17:20:39 +0800

    55 C was a little too hot for my tastes.

--- settings.py
+++ settings.py
@@ -23,10 +23,10 @@ class Settings:
 	)
 	
 	"""The background color of the icon. The key is the temperature in Celsius, and the value is the color."""
-	COLORS = {0: Qt.GlobalColor.cyan, 55: Qt.GlobalColor.yellow, 65: QColor('orange'), 90: Qt.GlobalColor.red}
+	COLORS = {0: Qt.GlobalColor.cyan, 50: Qt.GlobalColor.yellow, 65: QColor('orange'), 90: Qt.GlobalColor.red}
 	
 	"""The fan levels for SMART mode. The key is the temperature in Celsius, and the value is the fan level."""
-	LEVELS = {0: '0', 55: '1', 65: '3', 80: '7', 90: 'disengaged'}
+	LEVELS = {0: '0', 50: '1', 65: '3', 80: '7', 90: 'disengaged'}
 	
 	"""The time in seconds between updates of the thermal sensors and fan sensor."""
 	UPDATE_INTERVAL = 5
